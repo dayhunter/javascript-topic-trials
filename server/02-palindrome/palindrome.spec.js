0f6a12790e268fab277066c3fe720a96f60efe83
kaushik lala <kaushik.lala@za.ibm.com>
2017-02-02 15:55:45 +0200

    Added a function for palindrome

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,10 +1,16 @@
+function palindrome() {
+  return true;
+}
+
 describe('the palindrome canary test', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
   });
 
   describe('palindrome should be', () => {
-    it('yes for mom');
+    it('yes for mom', () => {
+      palindrome('mom').should.be.true();
+    });
 
     it('no for dude');
 

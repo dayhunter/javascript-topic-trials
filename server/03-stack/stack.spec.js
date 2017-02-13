d89e2587fd13bd9a0322f8d076944e042246f35b
WIL PANNELL <wgpannel@us.ibm.com>
2017-02-12 23:48:14 -0800

    fail spec for stack

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -30,7 +30,11 @@ describe.only('the stack spec', () => {
       stack.size().should.equal(1);
     });
 
-    it('leave stack empty when pushed and popped');
+    it('leave stack empty when pushed and popped', () => {
+      stack.push({});
+      stack.pop();
+      stack.isEmpty().should.be.true();
+    });
 
     it('overflow');
 

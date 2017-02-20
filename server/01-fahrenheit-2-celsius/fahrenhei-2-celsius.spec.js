da9d95f8035e6448767dbd1b37ee3aba86dc489d
Matt Oberlin <moberlin8@gmail.com>
2017-02-20 14:44:17 -0500

    simple test cases for fahrenheit to celsius conversion
    Signed-off-by: Matt Oberlin <moberlin8@gmail.com>

--- server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
@@ -1,8 +1,19 @@
 describe('the fahrenheit2celsius canary test', () => {
-  it('shows the infrastructure works');
+  it('shows the infrastructure works', () => {
+    true.should.be.true();
+  });
+
+  function fahrenheit2celsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+  }
 
   describe('fahrenheit2celsius should', () => {
-    it('give 0°C for 32°F');
-    it('give 10°C for 50°F');
+    it('give 0°C for 32°F', () => {
+      fahrenheit2celsius(32).should.equal(0);
+    });
+
+    it('give 10°C for 50°F', () => {
+      fahrenheit2celsius(50).should.equal(10);
+    });
   });
 });

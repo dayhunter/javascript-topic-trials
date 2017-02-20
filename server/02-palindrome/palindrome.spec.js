438c85ed0c7307d833d4d5a2f1c204fcd97eca35
Matt Oberlin <moberlin8@gmail.com>
2017-02-20 14:46:18 -0500

    defined test cases for palindrome
    Signed-off-by: Matt Oberlin <moberlin8@gmail.com>

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,10 +1,17 @@
-describe('the palindrome canary spec', () => {
+describe.only('the palindrome canary spec', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
   });
 
   describe('palindrome should be', () => {
     it('true for mom');
+    it('true for madam Im Adam');
     it('false for dude');
+    it('false for spaces');
+    it('false for whitespace');
+    it('false for empty string');
+    it('error for NULL');
+    it('error for nothing');
+    it('error for non-alphabetic characters');
   });
 });
